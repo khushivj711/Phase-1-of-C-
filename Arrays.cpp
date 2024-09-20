20b6fc342c125ff46afa6a979fd24c4585b75b69
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-20 16:44:53 +0000

    accessing elements

--- Arrays.cpp
+++ Arrays.cpp
@@ -3,8 +3,20 @@ using namespace std;
 
 
 int main(){
+
+    //declare 
     int number[15];
-    cout<<number[0]<<endl;
+    
+    //accessing an array
+    // cout<<number[0]<<endl;
+    // cout<<number[14];
+    
+    
+    //initialising an array
+    int second[3] = {1,23,4} ;
+
+    cout<<"Value at second 2 :" <<second[2];
+
     cout<<"Everything is fine" <<endl;
     return 0;
 }
