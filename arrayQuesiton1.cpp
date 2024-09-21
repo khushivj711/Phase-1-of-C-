96af803de7481ae41972bfea4079d0430e3580d7
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-21 02:39:19 +0000

    pre defined function to find out the min and max value in array

--- arrayQuesiton1.cpp
+++ arrayQuesiton1.cpp
@@ -4,28 +4,32 @@ using namespace std;
 //INT_MIN = minimum value padi h int ki
 //INT_MAX = maximum value padi h int ki by default
 
+//There is bydefault function to find out the max and min value in array
 int getMax(int num[] , int n){
 
-    int max = INT8_MIN;
+    int maxi = INT8_MIN;
 
     for(int i =  0 ; i<n ; i++){
-        if(num[i]>max){
-            max = num[i];
-        }
+
+        maxi = max(maxi , num[i]);
+        // if(num[i]>max){
+        //     max = num[i];
+        // }
     }
-    return max;
+    return maxi;
 }
 
 int getMin(int num[] , int n){
 
-    int min = INT8_MAX;
+    int mini = INT8_MAX;
 
     for(int i =  0 ; i<n ; i++){
-        if(num[i]<min){
-            min = num[i];
-        }
+        mini = min(mini , num[i]);
+        // if(num[i]<min){
+        //     min = num[i];
+        // }
     }
-    return min;
+    return mini;
 }
 
 int main(){
