9bf9401e7db3fbe346e2aa3a1c11e03c589bd3f5
khushivj711 <khushi.vijay8711@gmail.com>
2024-09-21 02:15:04 +0000

    array of double , float , bool

--- Arrays.cpp
+++ Arrays.cpp
@@ -12,7 +12,7 @@ void printArray(int arr[], int size){
 int main(){
 
     //declare 
-    int number[15];
+    // int number[15];
     
     //accessing an array
     // cout<<number[0]<<endl;
@@ -21,7 +21,7 @@ int main(){
     
     //initialising an array
     // int second[3] = {1,23,4} ;
-    int second[15] = {2,7};
+    // int second[15] = {2,7};
 
     // cout<<"Value at second 2 :" <<second[2];
 
@@ -34,10 +34,27 @@ int main(){
     //     cout<<fourth[i]<<" ";
     // }
 
-    printArray(second , 15);
-    cout<<"Everything is fine" <<endl;
+    // printArray(second , 15);
+    // cout<<"Everything is fine" <<endl;
+
+    //how to find size of array
+
+    // int secondSize = sizeof(second) / sizeof(int);
+    // cout<<secondSize;
 
 
 
+    char ch[15] = {'a' , 'b' , 'c' , 'd'};
+    cout<<ch[3];
+
+    for(int i = 0; i<15 ; i++){
+        cout<<ch[i]<<" " ;
+    }
+
+    double firstD[5] ;
+    float firstF[5];
+    bool firstB[5];
+
+    
     return 0;
 }
